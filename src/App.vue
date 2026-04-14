04362b29fe2e6305caec1cd9392e5788e5cddc89
selkniezpandora <samy.elkniez@groupe-pandora.com>
2026-04-14 15:49:43 +0200

    initialize Vue 3 project with TypeScript and Vite setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import MyComponent from "./components/MyComponent.vue";
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <MyComponent />
+</template>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+//Stores
+
+//Props
+
+//Refs
+
+//Models
+
+//Emits
+
+//Computeds
+
+//Functions
+
+</script>
+
+<template>
+<h2>Coucou !</h2>
+</template>
+
+<style scoped>
+
+</style>
