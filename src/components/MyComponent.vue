209d527e9e5b29f3d13bf2e785fd433de13574f5
selkniezpandora <samy.elkniez@groupe-pandora.com>
2026-04-14 17:18:55 +0200

    refactor MyComponent.vue to use options API and implement calculation method

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,19 +1,30 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts">
 
-const name = ref('Samy');
-const age = ref(20);
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      name: "Jean",
+    };
+  },
+  methods: {
+    calc(a: number, b: number): number {
+      return a + b;
+    },
+    func() {
+      return this.name.toUpperCase();
+    }
+  },
 
-
-function incrementAge() {
-    age.value++;
-}
+};
 
 </script>
 
 <template>
 <h2>Coucou !</h2>
-  {{ name }} a {{ incrementAge() }} ans.
+  {{ name }} a {{ calc(20, 5) }} ans.
+  {{ func() }}
+
 </template>
 
 <style scoped>
